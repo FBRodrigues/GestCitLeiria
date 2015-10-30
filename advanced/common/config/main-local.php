61f301cf9bc75e950c0ac9bcb21a1e0495edfa64
Fabio <2100954@my.ipleiria.pt>
2015-10-30 19:28:59 +0000

    2

--- advanced/common/config/main-local.php
+++ advanced/common/config/main-local.php
@@ -3,7 +3,7 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=mydb',
+            'dsn' => 'mysql:host=localhost;dbname=mydbsuper',
             'username' => 'root',
             'password' => '',
             'charset' => 'utf8',
