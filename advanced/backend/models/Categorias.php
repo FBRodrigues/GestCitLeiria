eef423de825838683ce92db58bfbcb50ac3cd21c
Fabio <2100954@my.ipleiria.pt>
2015-12-09 17:49:52 +0000

    categorias adicionado

---
+++ advanced/backend/models/Categorias.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "categorias".
+ *
+ * @property integer $idCategorias
+ * @property string $Valor
+ *
+ * @property Categorizacao[] $categorizacaos
+ */
+class Categorias extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'categorias';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['Valor'], 'string', 'max' => 45]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'idCategorias' => 'Id Categorias',
+            'Valor' => 'Valor',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCategorizacaos()
+    {
+        return $this->hasMany(Categorizacao::className(), ['Categorias_idCategorias' => 'idCategorias']);
+    }
+}
