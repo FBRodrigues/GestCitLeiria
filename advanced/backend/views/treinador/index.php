be07624f16d66ac25f95fe2464ffde83fd10e534
Fabio <2100954@my.ipleiria.pt>
2016-02-03 20:17:23 +0000

    last version ed

--- advanced/backend/views/treinador/index.php
+++ advanced/backend/views/treinador/index.php
@@ -24,7 +24,7 @@ $this->params['breadcrumbs'][] = $this->title;
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
         'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
+
 
             'idTreinador',
             'Nome',
